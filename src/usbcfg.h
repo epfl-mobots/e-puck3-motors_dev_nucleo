73df34c775adb0d1dc8106d21c9c51138e8c9429
EliotFerragni <eliot.ferragni@epfl.ch>
2019-03-01 16:13:33 +0100

    -Updated usbcfg accordingly with blackmagic submodule

--- src/usbcfg.h
+++ src/usbcfg.h
@@ -22,16 +22,30 @@
 #ifndef USBCFG_H
 #define USBCFG_H
 
+/*  Comment if you only want to have one USB serial port. 
+    You can't have two serial ports
+    if you don't have enough endpoints to run them
+    (we need 2 IN endpoints and 1 OUT endpoints for each serial port)
+*/
+//#define USE_TWO_USB_SERIAL
+
+//power asked to the computer in 2mA incerement (min 100mA, max 500mA)
+#define USB_POWER           50          //(100mA)
+
 #define USB_DEVICE_VID      0x1D50
 #define USB_DEVICE_PID      0x6018
 #define USB_VENDOR_NAME     "EPFL"
 #define USB_DEVICE_NAME     "Motors_dev_nucleo"
 #define USB_SERIAL_NUMBER   "EPUCK"
 #define USB_SERIAL1_NAME    "GDB Server"
+#ifdef USE_TWO_USB_SERIAL
 #define USB_SERIAL2_NAME    "Serial Monitor"
+#endif /* USE_TWO_USB_SERIAL */
 
 #define USB_GDB             SDU1
+#ifdef USE_TWO_USB_SERIAL
 #define USB_SERIAL          SDU2
+#endif /* USE_TWO_USB_SERIAL */
 
 typedef enum{
     GDB_INTERFACE = 0,
@@ -48,10 +62,13 @@ typedef enum{
 #define USB_DATA_SIZE                   0x40
 
 extern const USBConfig usbcfg;
-extern const SerialUSBConfig serusbcfg1;
-extern const SerialUSBConfig serusbcfg2;
 extern SerialUSBDriver SDU1;
+extern const SerialUSBConfig serusbcfg1;
+
+#ifdef USE_TWO_USB_SERIAL
+extern const SerialUSBConfig serusbcfg2;
 extern SerialUSBDriver SDU2;
+#endif /* USE_TWO_USB_SERIAL */
 
 /*
 * Initializes two serial-over-USB CDC drivers and starts and connects the USB.
