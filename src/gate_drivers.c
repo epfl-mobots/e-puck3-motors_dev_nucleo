ab5facb2c8afbb950e1637095d36e5fd8e4a8aa7
EliotFerragni <eliot.ferragni@epfl.ch>
2019-03-19 13:05:35 +0100

    -Gate drivers enhancement

--- src/gate_drivers.c
+++ src/gate_drivers.c
@@ -8,13 +8,14 @@
  
 #include "main.h"
 #include "gate_drivers.h"
-#include "drv8323.h"
+
+static thread_t* fault_handler = NULL;
 
 static SPIConfig spicfg = {
 	.circular = false,
 	.end_cb = NULL,
 	.ssline = 0,
-	.cr1 = SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0| SPI_CR1_CPHA,	//PRESCALER = 010, POL = 0, PHASE = 1
+	.cr1 = SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0 | SPI_CR1_CPHA,	//PRESCALER = 010, POL = 0, PHASE = 1
 	.cr2 = SPI_CR2_DS_3 | SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0	//16bits
 };
 
@@ -28,41 +29,90 @@ static DRV8323ConfRegisters drv8323cfg = {
 	.csa_control 	= SEN_LVL | CSA_GAIN_1 | VREF_DIV | CSA_FET
 };
 
-DRV8323Config drv8323_1 = {
-	.spip = &SPI_DRV8323,
-	.spicfg = &spicfg,
-	.enline = LINE_EN_DRIVER_1,
-	.ssline = LINE_CS_DRIVER_1_n,
-	.faultline = LINE_FAULT_DRIVER_1_n,
-	.registers = &drv8323cfg,
+static DRV8323Config gateDrivers[NB_OF_DRV8323] = {
+	{
+		.spip = &SPI_DRV8323,
+		.spicfg = &spicfg,
+		.enline = LINE_EN_DRIVER_1,
+		.ssline = LINE_CS_DRIVER_1_n,
+		.faultline = LINE_FAULT_DRIVER_1_n,
+		.registers = &drv8323cfg,
+	},
+	{
+		.spip = &SPI_DRV8323,
+		.spicfg = &spicfg,
+		.enline = LINE_EN_DRIVER_2,
+		.ssline = LINE_CS_DRIVER_2_n,
+		.faultline = LINE_FAULT_DRIVER_2_n,
+		.registers = &drv8323cfg,
+	},
+	{
+		.spip = &SPI_DRV8323,
+		.spicfg = &spicfg,
+		.enline = LINE_EN_DRIVER_3,
+		.ssline = LINE_CS_DRIVER_3_n,
+		.faultline = LINE_FAULT_DRIVER_3_n,
+		.registers = &drv8323cfg,
+	},
+	{
+		.spip = &SPI_DRV8323,
+		.spicfg = &spicfg,
+		.enline = LINE_EN_DRIVER_4,
+		.ssline = LINE_CS_DRIVER_4_n,
+		.faultline = LINE_FAULT_DRIVER_4_n,
+		.registers = &drv8323cfg,
+	}
 };
 
-DRV8323Config drv8323_2 = {
-	.spip = &SPI_DRV8323,
-	.spicfg = &spicfg,
-	.enline = LINE_EN_DRIVER_2,
-	.ssline = LINE_CS_DRIVER_2_n,
-	.faultline = LINE_FAULT_DRIVER_2_n,
-	.registers = &drv8323cfg,
-};
+void gateDriverCb(void* arg){
 
-DRV8323Config drv8323_3 = {
-	.spip = &SPI_DRV8323,
-	.spicfg = &spicfg,
-	.enline = LINE_EN_DRIVER_3,
-	.ssline = LINE_CS_DRIVER_3_n,
-	.faultline = LINE_FAULT_DRIVER_3_n,
-	.registers = &drv8323cfg,
-};
+	chSysLockFromISR();
+	//we set the bit at the position corresponding to the device number
+	//example : 0b00000000000000000000000000001000 corresponds to the device number 3
+	chEvtSignalI(fault_handler, (1 << (uint32_t)arg) );
+	chSysUnlockFromISR();
+}
 
-DRV8323Config drv8323_4 = {
-	.spip = &SPI_DRV8323,
-	.spicfg = &spicfg,
-	.enline = LINE_EN_DRIVER_4,
-	.ssline = LINE_CS_DRIVER_4_n,
-	.faultline = LINE_FAULT_DRIVER_4_n,
-	.registers = &drv8323cfg,
-};
+static THD_WORKING_AREA(gate_drivers_thd_wa, 128);
+static THD_FUNCTION(gate_drivers_thd, arg)
+{
+
+	(void) arg;
+	chRegSetThreadName("Gate drivers fault handler");
+
+	uint8_t device_id = 0;
+	uint16_t fault_1_reg = 0;
+	uint16_t fault_2_reg = 0;
+
+	while(1){
+		//we wait an infinite time for one fault event
+		eventmask_t evt = chEvtWaitOne(ALL_EVENTS);
+		
+
+		device_id = 0;
+
+		//this loop finds on many times the bit has been shifted, which gives the device number
+		while(evt > 0){
+			if(evt == 1){
+				break;
+			}else{
+				evt = evt >> 1;
+				device_id +=1;
+			}
+		}
+
+		fault_1_reg = drv8323ReadReg(&gateDrivers[device_id], FAULT_STATUS_1_REG);
+		fault_2_reg = drv8323ReadReg(&gateDrivers[device_id], FAULT_STATUS_2_REG);
+
+		/*
+		*	To do : do something with the fault detected
+		*/
+
+		//to remove the "variable unused" warning
+		(void)fault_1_reg;
+		(void)fault_2_reg;
+	}
+}
 
 void gateDriversEnable(DRV8323Config* drv){
 	palSetLine(drv->enline);
@@ -72,16 +122,33 @@ void gateDriversDisable(DRV8323Config* drv){
 	palClearLine(drv->enline);
 }
 
+void gateDriversWriteReg(gateDriver_id id, uint16_t reg){
+	drv8323ReadReg(&gateDrivers[id], reg);
+}
+
+uint16_t gateDriversReadReg(gateDriver_id id, uint16_t reg){
+	return drv8323ReadReg(&gateDrivers[id], reg);
+}
+
 void gateDriversInit(void){
-	gateDriversEnable(&drv8323_1);
-	gateDriversEnable(&drv8323_2);
-	gateDriversEnable(&drv8323_3);
-	gateDriversEnable(&drv8323_4);
+
+	fault_handler = chThdCreateStatic(gate_drivers_thd_wa, sizeof(gate_drivers_thd_wa), NORMALPRIO, gate_drivers_thd, NULL);
+
+	for(uint32_t i = 0 ; i < NB_OF_DRV8323 ; i++){
+		palSetLineCallback(gateDrivers[i].faultline, gateDriverCb, (void*) i);
+		gateDriversEnable(&gateDrivers[i]);
+	}
+
+	//we wait that the drivers are completely enabled and ready
 	chThdSleepMilliseconds(1);
-	drv8323WriteConf(&drv8323_1);
-	drv8323WriteConf(&drv8323_2);	
-	drv8323WriteConf(&drv8323_3);
-	drv8323WriteConf(&drv8323_4);
+
+	for(uint8_t i = 0 ; i < NB_OF_DRV8323 ; i++){
+		drv8323WriteConf(&gateDrivers[i]);
+		/* Enabling events on falling edge of the fault line.
+		 * We do it after the power on of the chips to not catch the fault pulse on init from the chip.
+		 */
+		palEnableLineEvent(gateDrivers[i].faultline, PAL_EVENT_MODE_FALLING_EDGE);
+	}
 }
 
 
--- src/gate_drivers.h
+++ src/gate_drivers.h
@@ -10,6 +10,21 @@
 #ifndef GATE_DRIVERS_H
 #define GATE_DRIVERS_H
 
+#include "drv8323.h"
 
+/**
+ * do not put more than 32 gate drivers, otherwise the event handling 
+ * made inside gate_drivers.c will be broken
+ */
+typedef enum {
+	DRV8323_1 = 0,
+	DRV8323_2,
+	DRV8323_3,
+	DRV8323_4,
+	NB_OF_DRV8323,
+} gateDriver_id;
+
+void gateDriversWriteReg(gateDriver_id id, uint16_t reg);
+uint16_t gateDriversReadReg(gateDriver_id id, uint16_t reg);
 void gateDriversInit(void);
 #endif /* GATE_DRIVERS_H */
