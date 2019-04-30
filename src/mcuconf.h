d0be78899660229c0ed08c142e5e886cbffc4fb1
Mohammed-Ismail Ben Salah <mohammed-ismail.bensalah@epfl.ch>
2019-04-30 16:16:13 +0200

    Made ADC IRQ great again

--- src/mcuconf.h
+++ src/mcuconf.h
@@ -246,7 +246,7 @@
 #define STM32_PWM_USE_TIM5                  FALSE
 #define STM32_PWM_USE_TIM8                  FALSE
 #define STM32_PWM_USE_TIM9                  FALSE
-#define STM32_PWM_TIM1_IRQ_PRIORITY         5
+#define STM32_PWM_TIM1_IRQ_PRIORITY         7
 #define STM32_PWM_TIM2_IRQ_PRIORITY         7
 #define STM32_PWM_TIM3_IRQ_PRIORITY         7
 #define STM32_PWM_TIM4_IRQ_PRIORITY         7
