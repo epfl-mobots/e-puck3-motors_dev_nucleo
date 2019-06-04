a4670d4c198a73e5f1bbc4f2f19761106e642ad9
Mohammed-Ismail Ben Salah <mohammed-ismail.bensalah@epfl.ch>
2019-06-04 16:00:52 +0200

    Refactor zero-crossing algo

---
+++ src/zero_cross.c
@@ -0,0 +1,126 @@
+/*
+ * zero_cross.c
+ *
+ *  Created on: 4 juin 2019
+ *      Author: Ismail-P51
+ */
+
+#include "zero_cross.h"
+
+ZCSDetect gZCS = {
+    .data_full = 0,
+    .data_idx  = 0,
+    .data_left = ZC_NB_POINTS,
+    // CST
+    .nb_channels = ZC_NUMBER_CHANNELS,
+    .nb_points = ZC_NB_POINTS
+};
+
+
+/*===========================================================================*/
+/* Zero-crossing detection                                                   */
+/*===========================================================================*/
+void Zcs_Reset_Struct(ZCSDetect* zcs)
+{
+  zcs->data_idx  = 0;
+  zcs->data_full = 0;
+  zcs->data_left = zcs->nb_points;
+}
+
+void Zcs_Insert_Data (ZCSDetect* zcs,uint16_t* input_data,size_t size)
+{
+  size_t i;
+
+  // Check if full
+  if(0 == zcs->data_left || 1 == zcs->data_full)
+  {
+    zcs->data_full = 1;
+    Zcs_Reset_Struct(zcs);
+  }
+
+  //
+  if(0==zcs->data_full)
+  {
+
+    // Enough space for all the data
+    if(zcs->data_left >= size)
+    {
+        for(i = 0;i<size;i++)
+        {
+          zcs->data[0][zcs->data_idx] = input_data[ zcs->nb_channels * i];
+          zcs->data[1][zcs->data_idx] = input_data[ zcs->nb_channels * i +1];
+          zcs->data[2][zcs->data_idx] = input_data[ zcs->nb_channels * i +2];
+          zcs->data[3][zcs->data_idx] = input_data[ zcs->nb_channels * i +3];
+          zcs->data_idx += 1;
+        }
+        zcs->data_left -= size;
+    }
+    // Fill it until maximum capacity
+    else
+    {
+      for(i = 0;i<zcs->data_left;i++)
+      {
+        zcs->data[0][zcs->data_idx] = input_data[ zcs->nb_channels * i];
+        zcs->data[1][zcs->data_idx] = input_data[ zcs->nb_channels * i +1];
+        zcs->data[2][zcs->data_idx] = input_data[ zcs->nb_channels * i +2];
+        zcs->data[3][zcs->data_idx] = input_data[ zcs->nb_channels * i +3];
+        zcs->data_idx += 1;
+      }
+      zcs->data_left -= zcs->data_left;
+    }
+
+  }
+
+
+}
+
+
+uint8_t Zcs_Detect(ZCSDetect* zcs)
+{
+
+  static volatile uint8_t  MeasurementArray[NB_STATE] = {0,1,2,0,1,2,0};
+  static volatile uint8_t  MeasurementArrayHigh[NB_STATE] = {0,0,0,2,2,1,1};
+  static volatile uint8_t  MeasurementArrayLow[NB_STATE] = {0,2,1,1,0,0,2};
+  static volatile uint8_t  MeasureChannel = 0;
+  int32_t lOldMeasure = 0;
+  int32_t lCurMeasure = 0;
+  uint8_t lChangeSign = 0; // 0 is FALSE, 1 is TRUE
+
+  static uint16_t lhighest_voltage = 0;
+  static uint16_t llowest_voltage = 0;
+  static uint16_t lhalf_bus = ZC_HALF_BUS;
+  static int32_t  lStateIterator = 0;
+
+  uint16_t ret_val = 0;
+
+  lStateIterator = brushcfg_GetStateIterator(&gBrushCfg);
+  MeasureChannel = MeasurementArray[lStateIterator];
+
+  lhighest_voltage = zcs->data[MeasurementArrayHigh[lStateIterator]][LATEST_DATA(zcs->data_idx)];
+  llowest_voltage = zcs->data[MeasurementArrayLow[lStateIterator]][LATEST_DATA(zcs->data_idx)];
+  lhalf_bus = LOW_PASS_COEFF_A * lhalf_bus + LOW_PASS_COEFF_B * (lhighest_voltage + llowest_voltage)/2;
+
+  // Check if the sign has changed between old measurement and actual
+  if(zcs->data_idx >  TWO_ELEM_IDX)
+  {
+    lCurMeasure = (int32_t) zcs->data[MeasureChannel][LATEST_DATA(zcs->data_idx)]   - (int32_t)(CORR_FACTOR_HALF_BUS * lhalf_bus);
+    lOldMeasure = (int32_t) zcs->data[MeasureChannel][PREVIOUS_DATA(zcs->data_idx)] - (int32_t)(CORR_FACTOR_HALF_BUS * lhalf_bus);
+    lChangeSign = ((lOldMeasure ^ lCurMeasure) < 0); // TRUE if sign has changed
+    brushcfg_SetZCFlag(&gBrushCfg); //gBrushCfg.ZCFlag |= lChangeSign;
+    ret_val = (MeasureChannel + 1)*lChangeSign;
+  }
+
+
+  if(ret_val > 0)
+  {
+    brushcfg_ComputeZCPeriod(&gBrushCfg);
+  }
+
+  return ret_val;
+
+}
+
+void zcs_ext_reset(void)
+{
+  Zcs_Reset_Struct(&gZCS);
+}
---
+++ src/zero_cross.h
@@ -0,0 +1,72 @@
+/*
+ * zero_cross.h
+ *
+ *  Created on: 4 juin 2019
+ *      Author: Ismail-P51
+ */
+
+#ifndef ZERO_CROSS_H_
+#define ZERO_CROSS_H_
+
+#include <ch.h>
+#include <hal.h>
+
+#include "tim1_motor.h"
+
+/*  ZERO-CROSSING DETECTION */
+#define ZC_NUMBER_CHANNELS      4
+
+
+#define ZC_NB_POINTS            1000
+#define ZC_THRESHOLD            925
+#define ZC_DELTA                100
+#define ZC_LOW_BOUND            ZC_THRESHOLD - ZC_DELTA
+#define ZC_HIGH_BOUND           ZC_THRESHOLD + ZC_DELTA
+#define ZC_SLOPE_PTS            4
+/* NEW */
+#define ZC_HALF_BUS             (int32_t) 925
+#define CORR_FACTOR_HALF_BUS    1.04f
+#define LOW_PASS_COEFF_A        0.99f
+#define LOW_PASS_COEFF_B        0.01f
+
+#define TWO_ELEM_IDX            1
+#define SIX_ELEM_IDX            7
+#define LATEST_DATA(x)          (x) - 1
+#define PREVIOUS_DATA(x)        (x) - 2
+
+#include "adc_motor.h"
+
+/*===========================================================================*/
+/* Typedefs                                                                  */
+/*===========================================================================*/
+typedef struct
+{
+  uint16_t data         [ZC_NUMBER_CHANNELS][ZC_NB_POINTS];
+  uint8_t  slope        [ZC_NUMBER_CHANNELS][ZC_NB_POINTS];
+  uint8_t  zero_crossing[ZC_NUMBER_CHANNELS];
+
+  // DATA MGT
+  const uint16_t nb_channels;
+  const uint16_t nb_points;
+  uint16_t data_left;
+  uint16_t data_idx;
+  uint8_t  data_full;
+
+}ZCSDetect;
+
+/*===========================================================================*/
+/* Prototypes                                                                */
+/*===========================================================================*/
+/* Zero crossing detection */
+void Zcs_Reset_Struct(ZCSDetect* zcs);
+void Zcs_Insert_Data (ZCSDetect* zcs,uint16_t* input_data,size_t size);
+uint8_t Zcs_Detect(ZCSDetect* zcs);
+void zcs_ext_reset(void);
+
+/*===========================================================================*/
+/* Export global variable                                                    */
+/*===========================================================================*/
+extern ZCSDetect gZCS;
+
+
+#endif /* ZERO_CROSS_H_ */
