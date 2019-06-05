f4f08ec5851908b70f4aa5542a9c0fa1086831c1
EliotFerragni <eliot.ferragni@epfl.ch>
2019-06-05 13:55:29 +0200

    -Added files for the AS5055 driver

---
+++ src/as5055.c
@@ -0,0 +1,30 @@
+/**
+ * @file	drv8323.c
+ * @brief  	File containing the driver to communicate by SPI with a DRV8323 gate driver
+ * 
+ * @written by  	Eliot Ferragni
+ * @creation date	14.03.2019
+ */
+ 
+#include "ch.h"
+#include "hal.h"
+#include "utility.h"
+#include "as5055.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/as5055.h
@@ -0,0 +1,15 @@
+/**
+ * @file	drv8323.h
+ * @brief  	File containing the driver to communicate by SPI with a DRV8323 gate driver
+ * 
+ * @written by  	Eliot Ferragni
+ * @creation date	14.03.2019
+ */
+ 
+
+#ifndef AS5055_H
+#define AS5055_H
+
+
+
+#endif /* AS5055_H */
