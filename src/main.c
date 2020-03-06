4e336c689299fce59225f335e7c266a7400bf55e
EliotFerragni <eliot.ferragni@epfl.ch>
2020-03-06 13:19:20 +0100

    -First draft of micropython working with ChibiOS

--- src/main.c
+++ src/main.c
@@ -99,6 +99,103 @@ static THD_FUNCTION(Thread1,arg) {
   }
 }
 
+#include "py/compile.h"
+#include "py/runtime.h"
+#include "py/repl.h"
+#include "py/gc.h"
+#include "py/mperrno.h"
+#include "lib/utils/pyexec.h"
+#include "mpconfigport.h"
+
+static char *stack_top;
+#if MICROPY_ENABLE_GC
+static char heap[120000];
+#endif
+
+void gc_collect(void) {
+    // WARNING: This gc_collect implementation doesn't try to get root
+    // pointers from CPU registers, and thus may function incorrectly.
+    void *dummy;
+    gc_collect_start();
+    gc_collect_root(&dummy, ((mp_uint_t)stack_top - (mp_uint_t)&dummy) / sizeof(mp_uint_t));
+    gc_collect_end();
+    gc_dump_info();
+}
+
+// Receive single character
+int mp_hal_stdin_rx_chr(void) {
+//     unsigned char c = 0;
+// #if MICROPY_MIN_USE_STDOUT
+//     int r = read(0, &c, 1);
+//     (void)r;
+// #elif MICROPY_MIN_USE_STM32_MCU
+//     // wait for RXNE
+//     while ((USART3->SR & (1 << 5)) == 0) {
+//     }
+//     c = USART3->DR;
+// #endif
+//     return c;
+
+    static uint8_t c[1] = {0};
+
+	chnRead((BaseChannel*)&USB_SERIAL, c, 1);
+
+	return (unsigned char)c[0];
+
+}
+
+// Send string of given length
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
+// #if MICROPY_MIN_USE_STDOUT
+//     int r = write(1, str, len);
+//     (void)r;
+// #elif MICROPY_MIN_USE_STM32_MCU
+//     while (len--) {
+//         // wait for TXE
+//         while ((USART3->SR & (1 << 7)) == 0) {
+//         }
+//         USART3->DR = *str++;
+//     }
+// #endif
+
+	if(len > 0){
+		chnWrite((BaseChannel*)&USB_SERIAL, (uint8_t*)str, len);
+	}
+}
+
+static THD_WORKING_AREA(waMicropythonThd,1024);
+static THD_FUNCTION(MicropythonThd,arg) {
+  	(void)arg;
+  	chRegSetThreadName("Micropython");
+
+
+  	int stack_dummy;
+	stack_top = (char*)&stack_dummy;
+
+#if MICROPY_ENABLE_GC
+	gc_init(heap, heap + sizeof(heap));
+#endif
+	mp_init();
+#if MICROPY_ENABLE_COMPILER
+	// Main script is finished, so now go into REPL mode.
+	// The REPL mode can change, or it can request a soft reset.
+	for (;;) {
+	    if (pyexec_mode_kind == PYEXEC_MODE_RAW_REPL) {
+	        if (pyexec_raw_repl() != 0) {
+	            break;
+	        }
+	    } else {
+	        if (pyexec_friendly_repl() != 0) {
+	            break;
+	        }
+	    }
+	}
+#else
+	pyexec_frozen_module("frozentest.py");
+#endif
+	mp_deinit();
+}
+
 /*===========================================================================*/
 /* Main.                                                       */
 /*===========================================================================*/
@@ -122,7 +219,7 @@ int main(void) {
 	initGDBEvents();
 	gdbStart();
 
-	usbPDControllerStart();
+	//usbPDControllerStart();
 
 	// Debug MCU Config
 	DBGMCU->APB2FZ |= DBGMCU_APB2_FZ_DBG_TIM1_STOP | DBGMCU_APB2_FZ_DBG_TIM8_STOP; // Clock and outputs of TIM 1 are disabled when the core is halted
@@ -131,57 +228,59 @@ int main(void) {
 	/* Debug IO for interrupt timings */
 	palSetLineMode(DEBUG_INT_LINE,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
 	palClearLine(DEBUG_INT_LINE);
-  palSetLineMode(DEBUG_INT_LINE2,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-  palClearLine(DEBUG_INT_LINE2);
-  palSetLineMode(DEBUG_INT_LINE3,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-  palClearLine(DEBUG_INT_LINE3);
-  palSetLineMode(DEBUG_INT_LINE4,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
-  palClearLine(DEBUG_INT_LINE4);
+	palSetLineMode(DEBUG_INT_LINE2,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+	palClearLine(DEBUG_INT_LINE2);
+	palSetLineMode(DEBUG_INT_LINE3,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+	palClearLine(DEBUG_INT_LINE3);
+	palSetLineMode(DEBUG_INT_LINE4,PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST);
+	palClearLine(DEBUG_INT_LINE4);
 
 	/*
 	* Initializes two serial-over-USB CDC drivers and starts and connects the USB.
 	*/
 	usbSerialStart();
 
-	shellInit();
-	gateDriversEnableAll();
-  gateDriversDisable(GATE_DRIVER_3);
+	//shellInit();
+	// gateDriversEnableAll();
+ //  	gateDriversDisable(GATE_DRIVER_3);
 
-  motorsStart();
+ //  	motorsStart();
 
-  encodersStartReading();
+ //  	encodersStartReading();
 
 	// Configure the Thread that will blink the leds on the boards
 	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, NULL);
 
-  motorSetDutyCycle(BRUSHLESS_MOTOR_1, 10);
-  motorSetDutyCycle(BRUSHLESS_MOTOR_2, 10);
-  motorSetDutyCycle(BRUSHLESS_MOTOR_3, 10);
-  motorSetDutyCycle(BRUSHLESS_MOTOR_4, 10);
-  
+	// motorSetDutyCycle(BRUSHLESS_MOTOR_1, 10);
+	// motorSetDutyCycle(BRUSHLESS_MOTOR_2, 10);
+	// motorSetDutyCycle(BRUSHLESS_MOTOR_3, 10);
+	// motorSetDutyCycle(BRUSHLESS_MOTOR_4, 10);
+  	uint8_t configured = 0;
 	while (true)
 	{
 		/* Send ADC values */
 
-		if(isUSBConfigured()){
+		if(isUSBConfigured() && !configured){
 			//spawns the shell if the usb is connected
-			spawn_shell();
+			//spawn_shell();
+			chThdCreateStatic(waMicropythonThd, sizeof(waMicropythonThd), NORMALPRIO, MicropythonThd, NULL);
+			configured = 1;
 		}
-		chThdSleepMilliseconds(100);
+		// chThdSleepMilliseconds(100);
+		chThdSleepMilliseconds(1000);
+  //   chprintf((BaseSequentialStream *)&USB_GDB, "rpm = %f, current = %f \r\n",motorsGetRPM(BRUSHLESS_MOTOR_2), motorsGetCurrent(BRUSHLESS_MOTOR_2));
 
-    chprintf((BaseSequentialStream *)&USB_GDB, "rpm = %f, current = %f \r\n",motorsGetRPM(BRUSHLESS_MOTOR_2), motorsGetCurrent(BRUSHLESS_MOTOR_2));
+  //   static float percent = 90;
 
-    static float percent = 90;
+  //   if(palReadLine(LINE_NUCLEO_USER_BUTTON)){
+  //     percent -= 5;
+  //     if(percent < 0){
+  //       percent = 90;
+  //     }
 
-    if(palReadLine(LINE_NUCLEO_USER_BUTTON)){
-      percent -= 5;
-      if(percent < 0){
-        percent = 90;
-      }
-
-      chprintf((BaseSequentialStream *)&USB_GDB, "duty cycle = %f\r\n",100-percent);
-      chThdSleepMilliseconds(500);
-    }
+  //     chprintf((BaseSequentialStream *)&USB_GDB, "duty cycle = %f\r\n",100-percent);
+  //     chThdSleepMilliseconds(500);
+  //   }
     //chprintf((BaseSequentialStream *)&USB_GDB,"step = %d\n",gBrushCfg.ZCPeriod);
     //chprintf((BaseSequentialStream *) &USB_GDB, "Hi = %d, Lo = %d, Angle 1 = %.2f, Hi = %d, Lo = %d, Angle 2 = %.2f\n",data[0].alarm_hi, data[0].alarm_lo, data[0].angle, data[1].alarm_hi, data[1].alarm_lo, data[1].angle);
     //chprintf((BaseSequentialStream *)&USB_GDB, "averages : %d %d %d %d %d %d\r\n",gBrushCfg.kChannelNeutralPoint[1], gBrushCfg.kChannelNeutralPoint[2],gBrushCfg.kChannelNeutralPoint[3],gBrushCfg.kChannelNeutralPoint[4],gBrushCfg.kChannelNeutralPoint[5],gBrushCfg.kChannelNeutralPoint[6]);
